138b49a7d471c57fcdd4f1ce8ea0cd6c4d6b4dc8
Luiz Freneda <lfreneda@gmail.com>
2015-07-02 16:05:00 -0300

    stubin people server api

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,15 +1,31 @@
+var json = [
+  { id: 1, name: "Luiz", title: "Developer"},
+  { id: 2, name: "Freneda", title: "Coder"},
+  { id: 3, name: "Perez", title: "Programmer"},
+  { id: 4, name: "Junior", title: "Intern"}
+];
+
 module.exports = function(app) {
     app.get('/api/people', getPeople);
+    app.get('/api/people/:id', getPerson);
+    app.post('/api/people', addPerson);
+    app.delete('/api/people/:id', deletePerson)
 
-    function getPeople(req, res, next) {
+    function addPerson(req, res) {
+      json.push(req.body);
+      res.send(req.body);
+    }
 
-        var json = [
-          { id: 1, name: "Luiz", title: "Developer"},
-          { id: 2, name: "Freneda", title: "Coder"},
-          { id: 3, name: "Perez", title: "Programmer"},
-          { id: 4, name: "Junior", title: "Intern"}
-        ];
-
+    function getPeople(req, res) {
         res.send(json);
+    }
+
+    function getPerson(req,res) {
+      res.send({ id: 4, name: "Junior", title: "Intern"});
+    }
+
+    function deletePerson(req, res) {
+      json.splice(0, 1);
+      res.send(json);
     }
 };
